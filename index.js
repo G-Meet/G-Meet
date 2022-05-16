137b353eba7fad1df4c491d02ea771e00d68ce8b
Joe2357 <deviljoe996@gmail.com>
2022-05-16 22:08:31 +0900

    code cleanup

--- index.js
+++ index.js
@@ -1,171 +1,116 @@
-
-
+/* module define section */
 var http = require("https");
 const express = require("express");
 const socket = require("socket.io");
 const app = express();
 const spawn = require('child_process').spawn;
+/* end of module section */
+
+// global variable
 var result;
 var userInfo = [];
-//Starts the server
 
+/* server open section */
 let server = app.listen(4000, function () {
-  console.log("Server is running");
-
-  fileRead();
+	console.log("Server is running");
+	fileRead(); // alternative of initialize
 });
-
 function fileRead() {
-  // fs(파일시스템) 모듈 사용
-  const readline = require('readline');
-  const fs = require('fs');
-  const { listenerCount } = require("process");
-  const userFileName = "./userInfo.txt";
+	// file module used
+	const readline = require('readline');
+	const fs = require('fs');
+	const { listenerCount } = require("process");
+	const userFileName = "./userInfo.txt";
 
-  var rl = readline.createInterface({
-    input: fs.createReadStream(userFileName),
-    output: process.stdout,
-    terminal: false
-  });
-  rl.on('line', function (text) {
-    if (text != "") {
-      var obj = new Object();
-      obj.name = text.split(" ")[0];
-      obj.password = text.split(" ")[1];
-      obj.room = text.split(" ")[2];
+	// read user info from txt file
+	var rl = readline.createInterface({
+		input: fs.createReadStream(userFileName),
+		output: process.stdout,
+		terminal: false
+	});
+	rl.on('line', function (text) {
+		if (text != "") {
+			var obj = new Object();
+			obj.name = text.split(" ")[0];
+			obj.password = text.split(" ")[1];
+			obj.room = text.split(" ")[2];
 
-      userInfo.push(obj);
-    }
-  });
+			userInfo.push(obj);
+		}
+	});
 }
-
-function checkUser() {
-  for (const user of userInfo) {
-    console.log("Username: " + user.name + ", Password: " + user.password + ", Room: " + user.room);
-  }
+function checkUser() { // debug function
+	for (const user of userInfo) {
+		console.log("Username: " + user.name + ", Password: " + user.password + ", Room: " + user.room);
+	}
 }
+/* end of server open section */
 
-
+/* server flow section */
 app.use(express.static("public"));
-
-//Upgrades the server to accept websockets.
-
 let io = socket(server);
 
-//Triggered when a client is connected.
+io.on("connection", function (socket) { // the set of WebRTC methods
+	console.log("User Connected :" + socket.id); // debug message
 
+	socket.on("connectStart", function (name, pass) { // login
+		for (const user of userInfo) {
+			if (user.name == name && user.password == pass) {
+				socket.emit("connectAgreed", user.room);
+				return;
+			}
+		}
+		socket.emit("connectReject");
+	});
+	socket.on("join", function (roomName) { // access granted
+		let rooms = io.sockets.adapter.rooms;
+		let room = rooms.get(roomName);
 
+		if (room == undefined) { // when nobody was in the room
+			socket.join(roomName);
+			socket.emit("created");
+		} else if (room.size == 1) { // when someone was in the room
+			socket.join(roomName);
+			socket.emit("joined");
+		} else { // when there is no seat
+			socket.emit("full");
+		}
+		console.log(rooms); // debug
+	});
 
-// var os = require('os');
-// var nodeStatic = require('node-static');
+	/* broadcast a user's message to all users */
+	socket.on("ready", function (roomName) { // when a user is ready to stream
+		socket.broadcast.to(roomName).emit("ready");
+	});
+	socket.on("candidate", function (candidate, roomName) { // find candidates
+		console.log(candidate);
+		socket.broadcast.to(roomName).emit("candidate", candidate);
+	});
+	socket.on("offer", function (offer, roomName) { // offer WebRTC request
+		socket.broadcast.to(roomName).emit("offer", offer);
+	});
+	socket.on("answer", function (answer, roomName) { // answer for the request
+		socket.broadcast.to(roomName).emit("answer", answer);
+	});
+	socket.on("leave", function (roomName) { // someone left from the room
+		socket.leave(roomName);
+		socket.broadcast.to(roomName).emit("leave");
+		console.log(io.sockets.adapter.rooms);
+	});
+	socket.on("sendingMessage", function (roomName, data) { // chatting
+		socket.broadcast.to(roomName).emit("broadcastMessage", data);
+	});
+	socket.on("startTimer", function (roomName, data) { // timer & detection start
+		socket.broadcast.to(roomName).emit("startTimer", data);
+		console.log("Python Module start");
 
-// const socketIO = require('socket.io');
-// const https = require('https');
-// const fs = require('fs');
-// const options = { 
-//   key: fs.readFileSync('./private.pem'), 
-//   cert: fs.readFileSync('./public.pem') 
-// }; 
-//   var fileServer = new(nodeStatic.Server)(); 
-//   let app = https.createServer(options, (req,res)=>{ 
-//     fileServer.serve(req, res); 
-//   }).listen(4000);
-
-/* peer가 서버에 접속시, main.js(클라이언트측)의 window.room = prompt("Enter room name:")에 의해
-사용자에게 room id를 얻고 var socket = id.connect()로 서버와 소켓 연결
-socket.emit('create or join',room)을 통해 서버에게 알림 */
-
-
-
-// console.log("starting server....");
-// var io = socketIO.listen(app);
-
-
-
-
-
-io.on("connection", function (socket) {
-  //checkUser();
-  console.log("User Connected :" + socket.id);
-
-  //Triggered when a peer hits the join room button.
-
-  socket.on("connectStart", function(name,pass) {
-    for (const user of userInfo) {
-      if (user.name == name && user.password == pass) {
-        socket.emit("connectAgreed", user.room);
-        return;
-      }
-    }
-    socket.emit("connectReject");
-  });
-
-  socket.on("join", function (roomName) {
-    let rooms = io.sockets.adapter.rooms;
-    let room = rooms.get(roomName);
-
-    //room == undefined when no such room exists.
-    if (room == undefined) {
-      socket.join(roomName);
-      socket.emit("created");
-    } else if (room.size == 1) {
-      //room.size == 1 when one person is inside the room.
-      socket.join(roomName);
-      socket.emit("joined");
-    } else {
-      //when there are already two people inside the room.
-      socket.emit("full");
-    }
-    console.log(rooms);
-  });
-
-  //Triggered when the person who joined the room is ready to communicate.
-  socket.on("ready", function (roomName) {
-    socket.broadcast.to(roomName).emit("ready"); //Informs the other peer in the room.
-  });
-
-  //Triggered when server gets an icecandidate from a peer in the room.
-
-  socket.on("candidate", function (candidate, roomName) {
-    console.log(candidate);
-    socket.broadcast.to(roomName).emit("candidate", candidate); //Sends Candidate to the other peer in the room.
-  });
-
-  //Triggered when server gets an offer from a peer in the room.
-
-  socket.on("offer", function (offer, roomName) {
-    socket.broadcast.to(roomName).emit("offer", offer); //Sends Offer to the other peer in the room.
-  });
-
-  //Triggered when server gets an answer from a peer in the room.
-
-  socket.on("answer", function (answer, roomName) {
-    socket.broadcast.to(roomName).emit("answer", answer); //Sends Answer to the other peer in the room.
-  });
-
-socket.on("leave", function(roomName){
-      socket.leave(roomName);
-      socket.broadcast.to(roomName).emit("leave");
-	console.log(io.sockets.adapter.rooms);
-  });
-
-  socket.on("sendingMessage", function (roomName, data) {
-    socket.broadcast.to(roomName).emit("broadcastMessage", data);
-  });
-  socket.on("startTimer", function(roomName,data){
-    socket.broadcast.to(roomName).emit("startTimer", data);
-    console.log("Python Module start");
-
-    result = spawn('python', ['./opencv/model.py']);
-
-    result.stdout.on('data', function(data) { console.log(data.toString()); });
-    result.stderr.on('data', function(data) { console.log(data.toString()); });
-  });
-  socket.on("stopTimer", function(roomName,data){
-    socket.broadcast.to(roomName).emit("stopTimer",data);
-  });
-  //console.log("Websocket Connected", socket.id);
+		result = spawn('python', ['./opencv/model.py']); // execute python from javascript
+		result.stdout.on('data', function (data) { console.log(data.toString()); });
+		result.stderr.on('data', function (data) { console.log(data.toString()); });
+	});
+	socket.on("stopTimer", function (roomName, data) { // timer ends
+		socket.broadcast.to(roomName).emit("stopTimer", data);
+	});
+	/* end of message broadcast section */
 });
-
-
-
+/* end of server flow section */
